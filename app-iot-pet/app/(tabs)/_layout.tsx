bf00814ee2d4641288a66b8f49d66070b7a752f8
manitchaya_3238 <chonticha.so@kkumail.com>
2025-08-09 02:00:15 +0700

    add menu

--- app-iot-pet/app/(tabs)/_layout.tsx
+++ app-iot-pet/app/(tabs)/_layout.tsx
@@ -1,12 +1,13 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -29,15 +30,36 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'หน้าหลัก',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="pet"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'สัตว์เลี้ยง',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="paw" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: 'แผนที่',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="map-pin" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="devices"
+        options={{
+          title: 'อุปกรณ์',
+          tabBarIcon: ({ color }) => <MaterialIcons name="devices" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'โปรไฟล์',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="user-pen" size={24} color={color} />,
         }}
       />
     </Tabs>
---
+++ app-iot-pet/app/(tabs)/devices.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Maps() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Devices</Text>
+    </View>
+  );
+}
